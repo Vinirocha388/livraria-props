7a0b390a434e2a42fb1c233597d762fe6d1e6945
Vinícius Rocha <vinicius.a.rocha8@aluno.senai.br>
2025-04-08 10:45:17 -0300

    feat: create bookCard component

--- src/components/BooksSection/index.jsx
+++ src/components/BooksSection/index.jsx
@@ -1,7 +1,8 @@
+import BooksCard from "../BookCard";
 import SectionHeader from "../SectionHeader";
 import styles from "./BooksSection.module.css";
 
-const BooksSection = ({books, renderStars, text, title, viewAllLink}) => {
+const BooksSection = ({books, renderStars,text, title, viewAllLink}) => {
   return (
     <section className={styles.bookSection}>
       <SectionHeader
@@ -12,40 +13,7 @@ const BooksSection = ({books, renderStars, text, title, viewAllLink}) => {
 
       <div className={styles.booksGrid}>
         {books.map((book) => (
-          <div key={book.id} className={styles.bookCard}>
-            <div className={styles.bookCover}>
-              <img
-                src={book.coverImage}
-                alt={book.title}
-                className={styles.coverImage}
-              />
-              <button className={styles.quickViewButton}>Visualizar</button>
-            </div>
-            <div className={styles.bookInfo}>
-              <h3 className={styles.bookTitle}>{book.title}</h3>
-              <p className={styles.bookAuthor}>por {book.author}</p>
-              <div className={styles.bookRating}>
-                {renderStars(book.rating)}
-                <span className={styles.ratingValue}>({book.rating})</span>
-              </div>
-              <div className={styles.bookTags}>
-                {book.tags.map((tag, index) => (
-                  <span key={index} className={styles.tag}>
-                    {tag}
-                  </span>
-                ))}
-              </div>
-              <div className={styles.bookPriceActions}>
-                <span className={styles.bookPrice}>
-                  R$ {book.price.toFixed(2)}
-                </span>
-                <div className={styles.bookActions}>
-                  <button className={styles.iconButton}>❤️</button>
-                  <button className={styles.iconButton}>🛒</button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <BooksCard key={book.id} book={book} renderStars={renderStars}/>
         ))}
       </div>
     </section>
